bdea36a48762cfe6696ad35e990b3312f52bbca8
Iykeprince <iykeprince48@gmail.com>
2020-06-10 00:05:45 +0100

    pushing flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -30,58 +30,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int _selectedPosition = -1;
-  
-  String _coffeePrice ="0";
 
-  int _cupsCounter =0;
+  String _coffeePrice = "0";
+
+  int _cupsCounter = 0;
 
   int price = 0;
 
-  String _currency ="₦";
+  String _currency = "₦";
 
-  static const String coffeeCup ="images/coffee_cup_size.png";
+  static const String coffeeCup = "images/coffee_cup_size.png";
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         title: FlatButton(
-          onPressed: (){
-            //TODO: Uncomment the _confirmOrderModalBottomSheet() method to show summary of order
-            //_confirmOrderModalBottomSheet(totalPrice: "$_currency$price", numOfCups: "x $_cupsCounter");
-          },
-          child: Text("Buy Now",style: TextStyle(color: Colors.black87),),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0), side: BorderSide(color: Colors.blue))
-        ),
+            onPressed: () {
+              //TODO: Uncomment the _confirmOrderModalBottomSheet() method to show summary of order
+              _confirmOrderModalBottomSheet(
+                  totalPrice: "$_currency$price", numOfCups: "x $_cupsCounter");
+            },
+            child: Text(
+              "Buy Now",
+              style: TextStyle(color: Colors.black87),
+            ),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.blue))),
         actions: [
           InkWell(
-            onTap: (){
+            onTap: () {
               //TODO: Uncomment the setState() function to clear the price and cups
               //TODO: Change the 'price' to 0 when this button is clicked
-              //setState(() {
-              this.price = -1;
-              this._cupsCounter =0;
-              // });
+              setState(() {
+                this.price = 0;
+                this._cupsCounter = 0;
+              });
             },
             child: Icon(Icons.clear),
           ),
           Container(
             height: double.maxFinite,
             alignment: Alignment.center,
-            child: Text("$_cupsCounter Cups = $_currency$price.00", style: TextStyle(fontSize: 18),),
+            child: Text(
+              "$_cupsCounter Cups = $_currency$price.00",
+              style: TextStyle(fontSize: 18),
+            ),
           )
         ],
       ),
-      body: Padding(padding: EdgeInsets.all(20), child: _mainBody(),) // This trailing comma makes auto-formatting nicer for build methods.
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: _mainBody(),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-
-  Widget _mainBody(){
+  Widget _mainBody() {
     return Container(
       height: double.maxFinite,
       width: double.maxFinite,
@@ -90,92 +98,127 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           Expanded(
             flex: 0,
-              child: Stack(
-            children: [
-              Container(
-                width: double.maxFinite,
-                height: 250,
-                margin: EdgeInsets.only(left: 50, right: 50, bottom: 50, top: 60),
-                decoration: BoxDecoration(borderRadius:
-                BorderRadius.all(Radius.circular(180)),
-                    color: Color.fromRGBO(239, 235, 233, 100)),
+            child: Stack(
+              children: [
+                Container(
+                  width: double.maxFinite,
+                  height: 250,
+                  margin:
+                      EdgeInsets.only(left: 50, right: 50, bottom: 50, top: 60),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(180)),
+                      color: Color.fromRGBO(239, 235, 233, 100)),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: double.maxFinite,
+                  height: 350,
+                  child: Image.asset(
+                    "images/cup_of_coffee.png",
+                    height: 300,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          Expanded(
+              flex: 0,
+              child: Text(
+                "Caffè Americano",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              )),
+          Padding(
+            padding: EdgeInsets.all(6),
+          ),
+          Expanded(
+            flex: 0,
+            child: Text(
+              "Select the cup size you want and we will deliver it to you in less than 48hours",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+                color: Colors.black45,
               ),
-              Container(
-                alignment: Alignment.center,
-                width: double.maxFinite,
-                height: 350,
-                child: Image.asset("images/cup_of_coffee.png", height: 300,),
-              )
-            ],
-          )),
-          Padding(padding: EdgeInsets.all(10),),
-          Expanded(flex: 0,child: Text("Caffè Americano",
-            style: TextStyle(fontWeight: FontWeight.bold,
-              fontSize: 30),)),
-          Padding(padding: EdgeInsets.all(6),),
-          Expanded(flex: 0, child: Text("Select the cup size you want and we will deliver it to you in less than 48hours",
-            style: TextStyle(fontWeight: FontWeight.bold,
-              fontSize: 14, color: Colors.black45,),
-            textAlign: TextAlign.start,),
+              textAlign: TextAlign.start,
+            ),
           ),
           Container(
             margin: EdgeInsets.only(top: 30, left: 20),
             height: 55,
             width: double.maxFinite,
             alignment: Alignment.center,
-            child:Row(
+            child: Row(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                RichText(text: TextSpan(
-                  text: _currency,
-                  style: TextStyle(fontWeight: FontWeight.bold,
-                      fontSize: 25, color: Colors.black87),
-                  children: [
-                    TextSpan(text: _coffeePrice, style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold))
-                  ]
-                ),),
+                RichText(
+                  text: TextSpan(
+                      text: _currency,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.black87),
+                      children: [
+                        TextSpan(
+                            text: _coffeePrice,
+                            style: TextStyle(
+                                fontSize: 50, fontWeight: FontWeight.bold))
+                      ]),
+                ),
                 Padding(
                   padding: EdgeInsets.only(right: 15),
                 ),
-                ListView.builder(itemBuilder: (context, index){
-
-                  return InkWell(
-                    child: _coffeeSizeButton(_selectedPosition == index,
-                        index ==0? "S" : index ==1? "M": "L"),
-                    onTap: (){
-                      setState(() {
-                        this._coffeePrice= index ==0? "300" : index ==1? "600": "900";
-                        _selectedPosition = index;
-                      });
-                    },
-                  );
-                }, scrollDirection: Axis.horizontal,
-                  itemCount: 3, shrinkWrap: true,),
-
-              ],),
+                ListView.builder(
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      child: _coffeeSizeButton(_selectedPosition == index,
+                          index == 0 ? "S" : index == 1 ? "M" : "L"),
+                      onTap: () {
+                        setState(() {
+                          this._coffeePrice =
+                              index == 0 ? "300" : index == 1 ? "600" : "900";
+                          _selectedPosition = index;
+                        });
+                      },
+                    );
+                  },
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 3,
+                  shrinkWrap: true,
+                ),
+              ],
+            ),
           ),
-
           Container(
             margin: EdgeInsets.only(top: 30),
             padding: EdgeInsets.all(10),
             width: double.maxFinite,
             height: 70,
-            child: FlatButton(onPressed: (){
-              //TODO: Currently _cupsCounter only show 1 when this button is clicked.
-              // TODO: Increment the _cupsCounter when 'Add to Bag' button is clicked'
-              //TODO: Call setState((){}) method to update both price and cups counter when 'Add to Bag' button is clicked
-
-              this._cupsCounter = 1;
-              this.price += int.parse(_coffeePrice);
-            }, child: Center(child: Text("Add to Bag",
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),)
-              ,),
+            child: FlatButton(
+              onPressed: () {
+                //TODO: Currently _cupsCounter only show 1 when this button is clicked.
+                // TODO: Increment the _cupsCounter when 'Add to Bag' button is clicked'
+                //TODO: Call setState((){}) method to update both price and cups counter when 'Add to Bag' button is clicked
+                
+                setState(() {
+                  this._cupsCounter += 1;
+                  this.price += int.parse(_coffeePrice);
+                });
+              },
+              child: Center(
+                child: Text(
+                  "Add to Bag",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
               color: Colors.blue,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50)
-              ),
+                  borderRadius: BorderRadius.circular(50)),
             ),
           )
         ],
@@ -183,35 +226,57 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _coffeeSizeButton(bool isSelected, String coffeeSize){
+  Widget _coffeeSizeButton(bool isSelected, String coffeeSize) {
     return Stack(
       children: [
-        Container(alignment: Alignment.center,  width: 55,
-          child: Text(coffeeSize, style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold,
-              color: isSelected? Colors.blue: Colors.black45),),),
+        Container(
+          alignment: Alignment.center,
+          width: 55,
+          child: Text(
+            coffeeSize,
+            style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+                color: isSelected ? Colors.blue : Colors.black45),
+          ),
+        ),
         new Container(
           margin: EdgeInsets.only(right: 10),
-          child: Image.asset(coffeeCup, width:50, color: isSelected ? Colors.blue: Colors.black45,),
-          decoration: BoxDecoration(border: Border(top: BorderSide(color: isSelected? Colors.blue: Colors.black45,
-              width: isSelected? 2: 1), left: BorderSide(color: isSelected? Colors.blue: Colors.black45,
-              width: isSelected? 2: 1), bottom: BorderSide(color: isSelected? Colors.blue: Colors.black45,
-              width: isSelected? 2: 1), right:  BorderSide(color: isSelected ?Colors.blue: Colors.black45 ,
-              width: isSelected? 2: 1)), borderRadius: BorderRadius.all(Radius.circular(5))),
+          child: Image.asset(
+            coffeeCup,
+            width: 50,
+            color: isSelected ? Colors.blue : Colors.black45,
+          ),
+          decoration: BoxDecoration(
+              border: Border(
+                  top: BorderSide(
+                      color: isSelected ? Colors.blue : Colors.black45,
+                      width: isSelected ? 2 : 1),
+                  left: BorderSide(
+                      color: isSelected ? Colors.blue : Colors.black45,
+                      width: isSelected ? 2 : 1),
+                  bottom: BorderSide(
+                      color: isSelected ? Colors.blue : Colors.black45,
+                      width: isSelected ? 2 : 1),
+                  right: BorderSide(
+                      color: isSelected ? Colors.blue : Colors.black45,
+                      width: isSelected ? 2 : 1)),
+              borderRadius: BorderRadius.all(Radius.circular(5))),
         )
       ],
     );
   }
 
-  void _confirmOrderModalBottomSheet({String totalPrice, String numOfCups}){
+  void _confirmOrderModalBottomSheet({String totalPrice, String numOfCups}) {
     showModalBottomSheet(
         context: context,
-        builder: (builder){
+        builder: (builder) {
           return new Container(
             height: 150.0,
             color: Colors.transparent, //could change this to Color(0xFF737373),
             //so you don't have to change MaterialApp canvasColor
             child: new Container(
-              padding: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
                 decoration: new BoxDecoration(
                     color: Colors.white,
                     borderRadius: new BorderRadius.only(
@@ -220,30 +285,43 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Column(
                   children: [
                     Container(
-                      child: Text("Confirm Order",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-              alignment: Alignment.center, height: 30, decoration: BoxDecoration(
-            ), ),
+                      child: Text(
+                        "Confirm Order",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      alignment: Alignment.center,
+                      height: 30,
+                      decoration: BoxDecoration(),
+                    ),
                     _getEstimate(totalPrice, numOfCups)
                   ],
                 )),
           );
-        }
-    );
+        });
   }
 
-  Widget _getEstimate(String totalPrice, String numOfCups){
+  Widget _getEstimate(String totalPrice, String numOfCups) {
     return Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Image.asset("images/cup_of_coffee.png", height: 70, width: 50,),
+        Image.asset(
+          "images/cup_of_coffee.png",
+          height: 70,
+          width: 50,
+        ),
         Padding(padding: EdgeInsets.all(10)),
-        Text(numOfCups, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+        Text(
+          numOfCups,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
         Padding(padding: EdgeInsets.all(10)),
-        Text(totalPrice, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),)
+        Text(
+          totalPrice,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        )
       ],
     );
   }
-
 }
